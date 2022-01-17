b13027a772038d95029c907b5465597c4077175e
Dev7518 <tryout.memoir.0x@icloud.com>
2022-01-17 15:49:35 +0100

    Data fetching from the API

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,16 +1,39 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFetch } from "../../utils/useFetch";
 
 export default function Feed() {
     const navigate = useNavigate();
+    const userKey = localStorage.getItem("lastLoggedInUser");
 
     useEffect(() => {
-        if (!localStorage.getItem("lastLoggedInUser")) navigate("/", {replace: true});
+        if (!userKey) navigate("/", { replace: true });
     }, []);
+
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            ReaderPublicKeyBase58Check: userKey,
+            FetchSubcomments: false,
+            GetPostsForFollowFeed: false,
+            GetPostsForGlobalWhitelist: false,
+            GetPostsByDESO: false,
+            MediaRequired: false,
+        })
+    };
+
+    const { data, error, isLoading } = useFetch('get-posts-stateless', requestOptions);
 
     return (
         <div>
-            <div className="container">this is feed page</div>
+            <div className="container">
+                <div>
+                    {!data && isLoading && "Loading..."}
+                    {!data && error && "Error..."}
+                    {data && data.PostsFound.map((x) => <div key={x.PostHashHex}>{x.Body}</div>)}
+                </div>
+            </div>
         </div>
     )
 }
